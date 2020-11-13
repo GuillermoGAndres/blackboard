74b7024924bb16f26cb28ef117e1b8fca36a1bc8
guillermoToshiba <memocampeon35@gmail.com>
2020-11-13 15:08:46 -0600

    Termine de ajustar el dibujo,agregue los botones de undo, borrow y changeColor con su funcionalidad perfectamente corriendo.

--- Blackboard.gd
+++ Blackboard.gd
@@ -1,8 +1,5 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var flag = false
 var evento
 var posicion_touch = Vector2(0,0)
@@ -14,6 +11,15 @@ var exitOneLine = false
 var indice = -1
 var lineaActual
 var nuevaLinea
+var terminarConteo = false
+var modoPencil = true
+var LineasBorrado = []
+var color = Color(21.0,74.0,175.0) # Color por default (es el apuntador que se ve) 
+var black = Color(0.0,0.0,0.0)
+var white = Color(1.0, 1.0, 1.0)
+var colorPencil = white
+var colorBorrow = black
+var change = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -48,7 +54,7 @@ func _ready():
 
 func _draw():
 	exitOneLine = false
-	var color = Color(0.0,0.0,0.0)
+	terminarConteo = false
 	var radius = 10.0; # Se puede hacer una interfaz que vaya cambiando el radius
 	draw_circle(posicion_touch, radius, color)
 	
@@ -57,7 +63,14 @@ func _draw():
 #	draw_primitive(point, color2, PoolVector2Array()) # third argument is UV, disregarded here, used to map textur
 #
 	print("Ya paso ready ahora me toca dibujarme")
+	print(indice)
 	lineas[indice].width = 5.0 # Ancho del pincel, default 10
+	lineas[indice].antialiased = true
+	lineas[indice].begin_cap_mode = 2 # Hace que sea rodondo el inicio de la linea
+	lineas[indice].end_cap_mode = 2 # Hace que sea rodondo el final de la linea
+	lineas[indice].joint_mode = 2
+	
+	#lineas[indice].default_color = Color(0.0, 0.0, 0.0)
 	lineas[indice].add_point(posicion_touch)
 	print("Puntos: ", lineas[indice].get_point_count() )
 	
@@ -74,8 +87,10 @@ func _process(delta):
 	#Quiero actualizar primero laposisiont
 #	update() # Va a llamar a la funcion draw
 	#print("Flujo programa")
-	count += 1*delta
-	print(count)
+	if !terminarConteo:
+		count += 1*delta
+		print(count)
+	
 	# Cuando el flujo del programa se vuelve contante significa que
 	# ya no hay mas eventos
 	# Si pasaron 1s podemos decir que despego el lapiz del papel
@@ -87,9 +102,15 @@ func _process(delta):
 		exitOneLine = true
 		nuevaLinea = Line2D.new()
 		add_child(nuevaLinea)
-		lineas.append(nuevaLinea)
 		indice += 1
+		lineas.append(nuevaLinea)
+		#lineas[indice] = nuevaLinea ya no sera necesaria
+		print(indice)
+		print(lineas)
 		# Esa linea que sea creo es la ira dibujada
+		# Ya no necesaria contar, contar nos ayudaba a saber el tiempo 
+		# para crear un nuevo lapiz
+		terminarConteo = true
 		pass
 	
 	pass
@@ -104,13 +125,28 @@ func _input(event):
 	# En el mundo de smartphone si entra es porque a fuerzas
 	# el evento sera el touch
 	# Lo que tendriamos que hacer es cada vez 
-	if event is InputEventScreenDrag : #or event is InputEventScreenTouch
+	if event is InputEventScreenDrag and modoPencil: #or event is InputEventScreenTouch
 		print("mouse button event at ", event.position)
 		posicion_touch = event.position
+		lineas[indice].default_color = colorPencil # cada linea que dibuje sera black
 		update()
 #	else: #esto me ayudo darme cuenta que es llamada solamente,cuando hay un evento
 #		print("solto", numLineas) 
 #		numLineas += 1
+#
+	# Esto significa que esta modo borrow y vamos eliminar puntos
+	if event is InputEventScreenDrag and !modoPencil:
+	# Para el borrado no he encontrda un algoritmo eficiente, en teoria
+	# tendria que  que se busqueda e ir buscando en calinea que se dibuje
+	# todas las posiciones de los puntos, ver si coincide con la posicion
+	# del touch y si coincide eleiminar ese punto, el problema sera la 
+	# busque en cada linea, porque tinenes miles de puntos
+	# ----
+	# La segunda solucion sera dibujar otra pero de diferente color
+		posicion_touch = event.position
+		lineas[indice].default_color = colorBorrow
+		update()
+		pass
 
 	# Esta variable es fundamental, sera nuestro lapso de tiempo que
 	# entre cuando suelta el lapiz o no, nos ayuda darnos una idea si es 
@@ -124,3 +160,44 @@ func _input(event):
 
 
 
+
+
+
+func _on_CheckButton_toggled(button_pressed):
+	if(button_pressed): # true when is on
+		print("Borrow")
+		modoPencil = false # significa que esta en modo borrow
+	else:
+		print("Pencil")
+		modoPencil = true
+	pass # Replace with function body.
+
+
+func _on_Button_pressed():
+	# Verifica exiten al menos una linea para escribir
+	# Significa que mas de una linea para eliminar
+	# se necesita al menos una linea para dibujar, por no se puede borrar
+	if indice > 1: 
+		# Se quedadn Deleted object en el array
+		lineas[indice].queue_free() #posicion se queda guardada null
+		lineas.remove(indice) # Elimina precisamente ese objeto
+		indice -= 1
+		print(indice)
+		print(lineas)
+		# Elimina todos los puntos, y ahora es la linea con la cual
+		# va a ser dibuja, dejando intacto los cambios de process
+		lineas[indice].clear_points() 
+	pass # Replace with function body.
+
+
+func _on_ChangeColorButton_pressed():
+	change = !change
+	if change:
+		colorPencil = black
+		colorBorrow = white
+		$ColorRect.color = white
+	else:
+		colorPencil = white
+		colorBorrow = black
+		$ColorRect.color = black
+	pass # Replace with function body.
